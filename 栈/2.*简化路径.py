9d53431a63436fcf99411c9fd1aa1505ee9e5d51
gaogep <zpf1893@qq.com>
2019-03-15 21:53:38 +0800

    -

---
+++ 栈/2.*简化路径.py
@@ -0,0 +1,29 @@
+# 以 Unix 风格给出一个文件的绝对路径，你需要简化它。或者换句话说，将其转换为规范路径
+
+
+class Solution(object):
+    def simplifyPath(self, path):
+        """
+        :type path: str
+        :rtype: str
+        """
+        path_stack = []
+        segments = path.split("/")
+        for segment in segments:
+            path_stack.append(segment)
+            if segment == "." or segment == '':
+                path_stack.pop()
+            elif segment == "..":
+                if len(path_stack) > 1:
+                    path_stack.pop()
+                    path_stack.pop()
+                else:
+                    path_stack.pop()
+
+        ans = "/".join(path_stack)
+        if not ans.startswith("/"):
+            ans = "/" + ans
+        # print "ans:", ans
+        return ans
+
+print(Solution.simplifyPath("/home/../zpf/"))
---
+++ 栈/3.二叉树中序遍历.py
@@ -0,0 +1,57 @@
+class Treeroot(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution(object):
+    def inorderTraversal(self, root):
+        """
+        :type root: Treeroot
+        :rtype: List[int]
+        """
+        res = []
+        if root == None:
+            return res
+        res += self.inorderTraversal(root.left)
+        res.append(root.val)
+        res += self.inorderTraversal(root.right)
+        return res
+
+    def inorderwithloop(self, root):
+        res = []
+        stack = []
+        while root or stack:
+            while root:
+                stack.append(root)
+                root = root.left
+            if stack:
+                tmp = stack.pop()
+                res.append(tmp.val)
+                root = tmp.right
+        return res
+
+    def postorderwithloop(self, root):
+        res = []
+        stack1 = []
+        stack2 = []
+        while root or stack1:
+            while root:
+                stack1.append(root)
+                stack2.append(root)
+                root = root.right
+            if stack1:
+                tmp = stack1.pop()
+                root = tmp.left
+        while stack2:
+            res.append(stack2.pop().val)
+        return res
+
+# T1 = Treeroot(1)
+# T2 = Treeroot(2)
+# T3 = Treeroot(3)
+# T1.right = T2
+# T2.left = T3
+
+s = Solution()
+print(s.postorder(T1))

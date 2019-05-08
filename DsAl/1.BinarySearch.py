6c933896b0ef47b64f841ad58f25fbbe1a83c9b0
gaogep <zpf1893@qq.com>
2019-05-08 22:04:06 +0800

    -

--- DsAl/1.BinarySearch.py
+++ DsAl/1.BinarySearch.py
@@ -1,7 +1,6 @@
 def binary_serach(arr, item):
     low = 0
     high = len(arr) - 1
-
     while low <= high:
         mid = (low + high) // 2
         if arr[mid] > item:
@@ -14,4 +13,4 @@ def binary_serach(arr, item):
 
 
 my_list = [1, 3, 5, 7, 9]
-print(binary_serach(my_list, 5))
+print(binary_serach(my_list, 9))

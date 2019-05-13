0d868a5a23f4bc4261e5bf64799b9615f7b5fda5
gaogep <zpf1893@qq.com>
2019-05-13 20:41:43 +0800

    -

--- 剑指offer/10.用两个队列实现栈.py
+++ 剑指offer/10.用两个队列实现栈.py
@@ -2,10 +2,10 @@ class qStack:
     def __init__(self):
         self.queue1 = []
         self.queue2 = []
-    
+
     def push(self, value):
         self.queue1.append(value)
-    
+
     def pop(self):
         ret = "The stack is empty"
         if self.queue1:
@@ -13,7 +13,3 @@ class qStack:
                 self.queue2.append(self.queue1.pop(0))
             ret = self.queue1.pop(0)
         return ret
-
-s = qStack()
-
-print(s.pop())
--- 剑指offer/9.用两个栈实现队列.py
+++ 剑指offer/9.用两个栈实现队列.py
@@ -2,10 +2,10 @@ class cQueue:
     def __init__(self):
         self.stack1 = []
         self.stack2 = []
-    
+
     def enqueue(self, value):
         self.stack1.append(value)
-    
+
     def dequeue(self):
         ret = "The queue is empty"
         if not self.stack2:

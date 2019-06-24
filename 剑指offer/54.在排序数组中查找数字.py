864b6a6c65c0266e8407bd8e1d035d25b96001e6
gaogep <zpf1893@qq.com>
2019-06-24 22:35:32 +0800

    -

---
+++ 剑指offer/54.在排序数组中查找数字.py
@@ -0,0 +1,48 @@
+# 数字在排序数组中出现的次数
+# 统计一个数字在排序数组中出现的次数
+# 二分查找 找到第一个和最后一个数字的位置 然后相减
+arr = [1, 2, 2, 3, 3, 3, 3, 4, 5]
+
+
+def getFirstValue(arr, k):
+    low = 0
+    high = len(arr) - 1
+    while low < high:
+        mid = (low + high) // 2
+        if arr[mid] > k:
+            high = mid - 1
+        elif arr[mid] < k:
+            low = mid + 1
+        else:
+            # 往前找
+            if arr[mid-1] != k:
+                return mid
+            elif arr[mid-1] == k:
+                high = mid
+
+
+def getLastValue(arr, k):
+    low = 0
+    high = len(arr) - 1
+    while low < high:
+        mid = (low + high) // 2
+        if arr[mid] > k:
+            high = mid - 1
+        elif arr[mid] < k:
+            low = mid + 1
+        else:
+            # 往后找
+            if arr[mid+1] != k:
+                return mid
+            elif arr[mid+1] == k:
+                low = mid
+
+
+def countNumberTimes(arr, k):
+    times = None
+    if arr:
+        times = getLastValue(arr, k) - getFirstValue(arr, k) + 1
+    return times
+
+
+print(countNumberTimes(arr, 3))
